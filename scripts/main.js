fcacba2927029bda43e488f224ebf5a3d949846d
KareemZuwa <kareemzuwa@gmail.com>
2020-12-08 13:41:17 +0100

    styling och functioner

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@
 const lista = document.querySelector('#search-wrapper_pic_list');
 const form = document.querySelector('.search-wrapper_form');
 const searchTerm = document.querySelector('#search');
+const key = '41adb0f120121d62ff61549e85feab86';
 
 //Form function för att skriva och söka ord
 form.addEventListener('submit', e => {
@@ -12,8 +13,9 @@ form.addEventListener('submit', e => {
 
 //Fetch function som hämtar data från Flickr API
 async function getData(query) {
-    const response = await fetch(`https://www.flickr.com/services/rest/?api_key=41adb0f120121d62ff61549e85feab86&method=flickr.photos.search&text=${query}&per_page=20&format=json&nojsoncallback=1`);
+    const response = await fetch(`https://www.flickr.com/services/rest/?api_key=${key}&method=flickr.photos.search&text=${query}&per_page=20&page=6&format=json&nojsoncallback=1`);
     const data = await response.json();
+    console.log(data);
 
     //console.log(data.photos.photo);   //consolar data från API
 
