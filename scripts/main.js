0a333f2e9444d34957064bb66caa81cc482f669b
KareemZuwa <kareemzuwa@gmail.com>
2020-12-12 23:06:28 +0100

    fixed bugs, responsiv and arrows still not working

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,9 @@ const searchTerm = document.querySelector('#search');               //search kna
 const key = '41adb0f120121d62ff61549e85feab86';                     //API nyckeln från flickr
 const perPage = document.querySelector('#per-page');                //option väljaren för hur många bilder som skall visas på en sida  
 const chooseSide = document.querySelectorAll('#links');             //länkar för att välja sida 
-const overLay = document.getElementById('search-wrapper_overlay');  //Overlay elementet      
+const overLay = document.getElementById('search-wrapper_overlay');  //Overlay elementet
+const leftArrow = document.querySelector('.left-arrow');             //vänster pil (prev knapp) 
+const rightArrow = document.querySelector('.right-arrow');          //höger pil (next knapp)  
 let pageSide= 1;                                                    //Genererar vald sida
 
 
@@ -40,8 +42,6 @@ async function getData(query) {
             return pageSide
             })
     });
-
-    console.log(data.photos.page)
     //console.log(data.photos.photo);   //consolar data från API
     //Skickar argument i form av json data till functionen showPhotos
     showPhotos(data.photos.photo);
@@ -56,30 +56,42 @@ function showPhotos(array) {
     array.forEach(value => {
         
         listUrl= `https://farm${value.farm}.staticflickr.com/${value.server}/${value.id}_${value.secret}_m.jpg`;
-        console.log(listUrl); //Sparar och consolar Url som skall generera bilder på webbappen.skrivs över med value värden från loopen
+        //console.log(listUrl); //Sparar och consolar Url som skall generera bilder på webbappen.skrivs över med value värden från loopen
         //listUrlBig=`https://farm${value.farm}.staticflickr.com/${value.server}/${value.id}_${value.secret}_b.jpg`;
 
         const item = document.createElement('li'); //skapar ny list element
 
         item.addEventListener('click', ()=> { //kalla lightbox funktionen
             lightBox(`https://farm${value.farm}.staticflickr.com/${value.server}/${value.id}_${value.secret}_b.jpg`, value.title);
-
         })
 
         item.classList.add('pictures'); //skapar classer på listelementen
         item.innerHTML = `<img src="${listUrl}" alt="${value.title}"></img>`; //skapar img taggar<p>${value.title}</p>
         lista.appendChild(item); //lägger till img items på listan
-        //console.log(value.title);
+        //console.log(lista);
+        //prev och Next knapparna på lightboxen
+        //sliderArrows(array);
+
     });
 };
-//overlay lightbox
-
+//Overlay--Lightbox--
 let lightBox = (value, title)=> {
-    console.log(title);
-    overLay.innerHTML = `<img src="${value}" alt="${title}"><p>${title}</p></img>`; //skapar lightbox image
+    const lightBoxPic = document.getElementById('image-wrapper');//tagg för blocket där stora bilden läggs 
+    lightBoxPic.innerHTML = `<img src="${value}" alt="${title}"><p>${title}</p></img>`; //skapar lightbox image
     overLay.classList.toggle('search-wrapper_overlay-hide');
-    overLay.addEventListener('click', ()=> {
-        overLay.classList.add('search-wrapper_overlay-hide');
-        
+    lightBoxPic.addEventListener('click', ()=> {
+        overLay.classList.add('search-wrapper_overlay-hide'); 
     })
+    /*
+    let currentImage = lightBoxPic;
+    console.log(currentImage);
+    */
 }
+leftArrow.addEventListener('click', ()=> {
+    console.log('left')
+    lightBoxPic
+});
+
+
+
+
